831957b468b1ccfc4af4fb2ba64d10d1826249ee
Matt Keys <matt@uptrending.com>
2014-01-29 13:58:08 -0500

    new cache busting
    Signed-off-by: Matt Keys <matt@uptrending.com>

--- lib/asset-cache-busting.php
+++ lib/asset-cache-busting.php
@@ -7,9 +7,9 @@ function fabric_rewrite_asset_query_var($content) {
 	global $wp_rewrite;
 	
 	$fabric_new_non_wp_rules = array(
-		'(.+)\.(.+)\.(js|css)$' => '$1.$3 [L]'
+		'(.+)\.(fabric_[0-9].+)\.(js|css)$' => '$1.$3 [L]'
 	);
-	
+
 	$wp_rewrite->non_wp_rules = array_merge($wp_rewrite->non_wp_rules, $fabric_new_non_wp_rules);
 	
 	return $content;
@@ -17,15 +17,22 @@ function fabric_rewrite_asset_query_var($content) {
 add_action('generate_rewrite_rules', 'fabric_rewrite_asset_query_var');
 
 function fabric_move_asset_version( $src ) {
+
+	// Return if pretty permalinks not enabled
+	if ( !get_option('permalink_structure') )
+		return $src;
+
 	// Don't touch admin scripts
 	if ( is_admin() )
 		return $src;
 	
-	return preg_replace(
+	$return = preg_replace(
 		'/\.(js|css)\?ver=(.+)$/',
-		'.$2.$1',
+		'.fabric_$2.$1',
 		$src
 	);
+
+	return $return;
 }
 add_filter( 'script_loader_src', 'fabric_move_asset_version' );
 add_filter( 'style_loader_src', 'fabric_move_asset_version' );
