eedd368a82e58e2e01d509f77abe1b79ba44aed1
Matt Keys <matt@uptrending.com>
2014-03-25 18:48:43 -0400

    renaming head.php to be more clear on its usage. removing test data files that should not have been committed.
    Signed-off-by: Matt Keys <matt@uptrending.com>

--- views/wrapper.php
+++ views/wrapper.php
@@ -2,7 +2,7 @@
 
 <body <?php body_class(); ?>>
 
-	<?php $view->get_template_part('head'); ?>
+	<?php $view->get_template_part('header-layout'); ?>
 
 	<!--[if lt IE 9]>
 		<div class="alert alert-warning"><?php _e( 'You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'fabric' ); ?></div>
